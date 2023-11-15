5a8c5212d0ebe4ae9a8c410f97948391f7671b25
Jeffrey Brown <jeffrey@mcgaw.io>
2023-11-15 10:36:23 -0500

    webpack dev server and build config

---
+++ config/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/main.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+		publicPath: '/',
+	},
+	module: {
+		rules: [
+			{
+        test: /\.js?$/,
+				exclude: /node_modules/, //don't test node_modules folder
+				use: {
+					loader: 'babel-loader',
+				},
+      },
+      {
+      	test: /\.svelte$/,
+        use: {
+          loader: 'svelte-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(jpg|jpeg|png|svg)$/,
+        use: 'file-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.mjs', '.js', '.svelte'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../public/index.html'),
+    }),
+    new MiniCssExtractPlugin(),
+    new Dotenv(),
+  ],
+	devServer: {
+		// historyApiFallback: true,
+		// contentBase: path.resolve(__dirname, 'dist'),
+		// hot: true,
+	},
+	resolve: {
+    conditionNames: ['require', 'svelte'],
+  },
+};
